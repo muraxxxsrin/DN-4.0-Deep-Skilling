c0657f951c66d98768c4d5d245e9215b9d8aa211
bmuralisridharan <b.muralisridharan@gmail.com>
2025-06-19 11:23:23 +0530

    Completed

--- Week1/Exercise 1/SingletonPatternExample/Main.java
+++ Week1/Exercise 1/SingletonPatternExample/Main.java
@@ -1,12 +1,30 @@
-
+import java.util.*;
 public class Main{
     public static void main(String [] args){
+        Date date = new Date();
         Logger log1 = Logger.getInstance();
         Logger log2 = Logger.getInstance();
+        int i=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type exit to stop loggind!\t");
+        while(true){
+            i+=1;
+            System.out.println("Enter a message: ");
+            String message = sc.nextLine();
+            if (i%2!=0){
+                System.out.println("Using Logger 1:");
+                log1.logs(message);
+            }else{
+                System.out.println("Using Logger 2:");
+                log2.logs(message);
+            }
+            System.out.println("\t");
 
-        log1.logs("Initial test");
-        log2.logs("Secondary test");
-        log2.logs("Final test");
+            if(message.equalsIgnoreCase("exit")){
+                log1.logs("Existing utility class : "+date);
+                break;
+            }
+        }
  
         
     }
